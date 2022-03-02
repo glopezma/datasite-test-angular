137dad74a1cb0adc6f903dd46f849b53edf800e3
Gabe Lopez <galopezmatthews@gmail.com>
2022-03-01 19:10:57 -0500

    added some simple tests to app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,69 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MemoizedSelector, Store } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { first, skip, take } from 'rxjs';
 import { AppComponent } from './app.component';
+import { UserState } from './store/users/models/user-state.interface';
+import {
+  selectAllUsers,
+  selectIsGettingUsers,
+} from './store/users/user.selectors';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+  let fixture: ComponentFixture<AppComponent>;
+  let store: MockStore<UserState>;
+  let initialState: UserState = {
+    users: [],
+    isGettingUsers: false,
+    error: null,
+  };
+  let mockIsGettingUsersSelector: MemoizedSelector<UserState, boolean>;
+  let mockUsersSelector: MemoizedSelector<UserState, any>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      providers: [provideMockStore({ initialState })],
     }).compileComponents();
+
+    store = TestBed.get<Store>(Store);
+    mockIsGettingUsersSelector = store.overrideSelector(
+      selectIsGettingUsers,
+      false
+    );
+    mockUsersSelector = store.overrideSelector(selectAllUsers, []);
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'datasite-test-angular'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should show loading until data retrieved', () => {
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('datasite-test-angular');
+    mockIsGettingUsersSelector.setResult(true);
+    store.refreshState();
+    fixture.detectChanges();
+    app.loading$.pipe(take(1)).subscribe((loading) => {
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(loading).toBe(true);
+      expect(compiled.querySelector('.loading')?.textContent).toContain(
+        'Loading...'
+      );
+    });
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should not show loading when data retrieved', () => {
+    const app = fixture.componentInstance;
+    mockIsGettingUsersSelector.setResult(false);
+    store.refreshState();
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('datasite-test-angular app is running!');
+    app.loading$.pipe(first()).subscribe((loading) => {
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(loading).toBe(false);
+      expect(compiled.querySelectorAll('.loading').length).toBe(0);
+    });
   });
 });
